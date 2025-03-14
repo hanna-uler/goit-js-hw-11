a080600907324122ec9baefcc2b9989d110fca8e
hanna-uler <hannauler@gmail.com>
2025-03-14 16:45:34 -0400

    add markup, started main.js

---
+++ src/main.js
@@ -0,0 +1,17 @@
+
+
+const formEl = document.querySelector(".form");
+formEl.addEventListener("submit", onSubmit);
+
+// restoreInput();
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.search-text.value);
+// if (event.currentTarget.search-text.value.trim("") === "") {
+    
+// } else {
+    
+// }
+    // some code
+};
