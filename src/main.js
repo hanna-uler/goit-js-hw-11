02029ab541ddfd099d68b11f87c6d435b696b3ae
hanna-uler <hannauler@gmail.com>
2025-03-17 12:31:31 -0400

    add no-results-message

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ formEl.addEventListener("submit", onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
+    let picsArray = [];
     // console.log(event.currentTarget.searchText.value);
     if (event.currentTarget.searchText.value === "") {
         return iziToast.error({
@@ -25,6 +26,16 @@ function onSubmit(event) {
         })
     } else {
         const queryWords = event.currentTarget.searchText.value;
-        getPics(queryWords);
+        const picsArray = getPics(queryWords);
+    }
+    if (picsArray.length === 0) {
+      return iziToast.error({
+            theme: "dark",
+            message: "Sorry, there are no images matching your search query. Please try again!",
+            backgroundColor: "#EF4040",
+            closeOnClick: true,
+            position: "topRight",
+            timeout: 3000,
+        })  
     }
 };
