3c3027ac3973e44222972e64bccd1fd1f8a76e29
hanna-uler <hannauler@gmail.com>
2025-03-17 14:26:48 -0400

    add markup template to render func, add simpleLightbox feature

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,9 +5,32 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 export default function renderGallery (picsArray) {
     const galleryEl = document.querySelector(".gallery");
+    galleryEl.innerHTML = "";
     const galleryMarkup = picsArray
-        .map(image => `<li class="gallery-item"><a class="gallery-link" href="${image.largeImageURL}"><img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}"/></a></li>`)
+        .map(image => `<li class="gallery-item"><a class="gallery-link" href="${image.largeImageURL}"><img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}"/></a><ul class="img-info-list">
+            <li class="img-info-item">
+              <h3 class="img-info-item-header">Likes</h3>
+              <p class="img-info-item-value">${image.likes}</p>
+            </li>
+            <li class="img-info-item">
+              <h3 class="img-info-item-header">Views</h3>
+              <p class="img-info-item-value">${image.views}</p>
+            </li>
+            <li class="img-info-item">
+              <h3 class="img-info-item-header">Comments</h3>
+              <p class="img-info-item-value">${image.comments}</p>
+            </li>
+            <li class="img-info-item">
+              <h3 class="img-info-item-header">Downloads</h3>
+              <p class="img-info-item-value">${image.downloads}</p>
+            </li>
+          </ul></li>`)
         .join("");
     
  galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);   
-}
+const slGalleryOptions = {
+  captionDelay: 250,
+  captionsData: "alt",
+};
+let slGallery = new SimpleLightbox('.gallery a', slGalleryOptions);
+};
